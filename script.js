c5acdc239135c940824a04a264daa23362579f3b
LouisTCM <156292333+LouisTCM@users.noreply.github.com>
2024-05-24 16:08:59 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const consoleSignatureStyle = "font-size: 16px;" +
+  "background: #ff6d92 linear-gradient(to bottom left, #0090FF 0, #0D1520 75%);" +
+  "color: white;" +
+  "text-align: center;" +
+  "padding: 10px 15px;" +
+  "width: 100%;" +
+  "border-radius: 20px;";
+const consoleSignatureText = "%cCarte de visite de Louis TORCHET-MAILLARD";
+console.log(consoleSignatureText, consoleSignatureStyle);
